0de92c2e0c0fb701dfb3ead2b768e1555cc8c21d
kwak chan <sgcks@naver.com>
2021-02-02 01:05:26 +0900

    post게시판 image, video upload

--- app/src/main/java/com/example/androidsns/WriteInfo.java
+++ app/src/main/java/com/example/androidsns/WriteInfo.java
@@ -2,23 +2,31 @@ package com.example.androidsns;
 
 import android.widget.EditText;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class WriteInfo {
     private String title;
-    private String contents;
+    private ArrayList<String> contents;
     private String publisher;
+    private Date createdAt;
 
-    public WriteInfo(String title, String contents, String publisher){
+    public WriteInfo(String title, ArrayList<String> contents, String publisher, Date createdAt){
         this.title = title;
         this.contents = contents;
         this.publisher = publisher;
+        this.createdAt = createdAt;
     }
 
     public String getTitle() { return this.title; }
     public void setTitle(String title) { this.title = title; }
 
-    public String getContents() { return this.contents; }
-    public void setContents(String contents) { this.contents = contents; }
+    public ArrayList<String> getContents() { return this.contents; }
+    public void setContents(ArrayList<String> contents) { this.contents = contents; }
 
     public String getPublisher() { return this.publisher; }
     public void setPublisher(String publisher) { this.publisher = publisher; }
+
+    public Date getCreatedAt() { return this.createdAt; }
+    public void setCreatedAt(Date createdAt) { this.createdAt = createdAt; }
 }
